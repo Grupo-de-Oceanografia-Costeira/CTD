5827db7ce301b3a1395eee1360d74f9a160bcd26
Arnaldo Russo <arnaldorusso@gmail.com>
2017-01-13 16:34:19 -0200

    Initial setup - CTD

---
+++ ctd/ctd.ino
@@ -0,0 +1,102 @@
+#include <SPI.h>
+#include <SD.h>
+// Base name must be six characters or less for short file names.
+#define FILE_BASE_NAME "Data"
+
+
+const uint8_t CS_PIN = 10; // ChipSelect (MKRZero SD: SDCARD_SS_PIN)
+const int gLed =  11;      //
+const int rLed = 8;        //
+const int LM35 = A0;       // Temperature sensor
+
+File dataFile;
+unsigned long int time;
+float temp;
+
+const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
+char fileName[] = FILE_BASE_NAME "00.csv";
+
+void setup(){
+  pinMode(rLed, OUTPUT);
+  pinMode(gLed, OUTPUT);
+  Serial.begin(9600);
+  
+  if (!SD.begin(CS_PIN)) {
+    Serial.println(F("begin failed"));
+    digitalWrite(rLed, HIGH);
+    return;
+  }
+  while (SD.exists(fileName)) {
+    if (fileName[BASE_NAME_SIZE + 1] != '9') {
+      fileName[BASE_NAME_SIZE + 1]++;
+    } else if (fileName[BASE_NAME_SIZE] != '9') {
+      fileName[BASE_NAME_SIZE + 1] = '0';
+      fileName[BASE_NAME_SIZE]++;
+    } else {
+      Serial.println(F("Can't create file name"));
+      digitalWrite(rLed, LOW);
+      delay(1000);
+      digitalWrite(rLed, HIGH);
+      delay(1000);
+      digitalWrite(rLed, LOW);
+      delay(1000);
+      digitalWrite(rLed, HIGH);
+      delay(1000);
+      digitalWrite(rLed, LOW);
+      delay(1000);
+      digitalWrite(rLed, HIGH);
+      return;
+    }
+  }
+  dataFfile = SD.open(fileName, FILE_WRITE);
+  if (!dataFile) {
+    Serial.println(F("open failed"));
+    digitalWrite(rLed, LOW);
+    delay(200);
+    digitalWrite(rLed, HIGH);
+    delay(200);
+    digitalWrite(rLed, LOW);
+    delay(200);
+    digitalWrite(rLed, HIGH);
+    delay(200);
+    digitalWrite(rLed, LOW);
+    return;
+  }
+  Serial.print(F("opened: "));
+  Serial.println(fileName);
+  digitalWrite(gLed,HIGH);
+  delay(2000);
+  digitalWrite(gLed, LOW);
+  delay(500);
+  digitalWrite(gLed,HIGH);
+  delay(2000);
+  digitalWrite(gLed, LOW);
+  dataFile.close();
+}
+
+void loop(){
+  time = millis();
+  temp = (float(analogRead(LM35))*5/(1023))/0.01;
+  if (temp > 28) {
+    digitalWrite(rLed, HIGH);
+    delay(250);
+    digitalWrite(rLed, LOW);
+    delay(250);
+  } 
+  else {
+    digitalWrite(gLed,HIGH);
+    delay(250);
+    digitalWrite(gLed, LOW);
+    delay(250);
+  }
+  File dataFile = SD.open(fileName, FILE_WRITE);
+  dataFile.print(temp); 
+  dataFile.print(",");
+  dataFile.print(temp);
+  dataFile.close();
+  Serial.print("The Temp is: ");
+  Serial.println(temp);
+  Serial.print("The time is: ");
+  Serial.println(time);
+  Serial.println("");
+}
